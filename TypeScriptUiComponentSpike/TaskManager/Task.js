2d5880ba9271bc985fd48ca5c16db34f051d0610
azecevic <azecevic@startgroup.com.au>
2016-04-14 20:04:35 +1000

    Added Skeleton. Impelmented ticking off of tasks and task count.

--- TypeScriptUiComponentSpike/TaskManager/Task.js
+++ TypeScriptUiComponentSpike/TaskManager/Task.js
@@ -11,23 +11,41 @@ var Task = (function () {
      * @param {HTMLElement} An element that should act as a container for the task's UI.
      * @param {string} A description of the task.
      */
-    function Task(container, description) {
+    function Task(container, taskDto, onCompleteChanged) {
         var _this = this;
         this.container = container;
-        this.description = description;
-        container.innerHTML = "\n<span>" + description + "</span>\n<input type=\"text\" value=\"" + description + "\" />\n<button data-role=\"edit\">Edit</button>\n<button data-role=\"save\">Save</button>\n<button data-role=\"cancel\">Cancel</button>\n";
+        this.taskDto = taskDto;
+        this.onCompleteChanged = onCompleteChanged;
+        container.innerHTML = "\n<input type=\"checkbox\" class=\"one column\" />\n<span class=\"seven columns\">" + taskDto.description + "</span>\n<input type=\"text\" class=\"seven columns\" value=\"" + taskDto.description + "\" />\n<button data-role=\"edit\" class=\"four columns\">Edit</button>\n<button data-role=\"save\" class=\"two columns\">Save</button>\n<button data-role=\"cancel\" class=\"two columns\">Cancel</button>\n";
+        this.completeInput = container.querySelector("input[type=checkbox]");
         this.descriptionSpan = container.querySelector("span");
-        this.descriptionInput = container.querySelector("input");
+        this.descriptionInput = container.querySelector("input[type=text]");
         this.editButton = container.querySelector("button[data-role=edit]");
         this.saveButton = container.querySelector("button[data-role=save]");
         this.cancelButton = container.querySelector("button[data-role=cancel]");
         this.saveButton.style.display = "none";
         this.cancelButton.style.display = "none";
         this.descriptionInput.style.display = "none";
+        this.completeInput.checked = taskDto.complete;
+        this.completeInput.addEventListener("click", function () { return _this.completeChanged(); });
         this.editButton.addEventListener("click", function () { return _this.beginEdit(); });
         this.saveButton.addEventListener("click", function () { return _this.save(); });
         this.cancelButton.addEventListener("click", function () { return _this.cancelEdit(); });
     }
+    Object.defineProperty(Task.prototype, "complete", {
+        /*
+         * Gets a value indicating whether or not the task is complete.
+         */
+        get: function () {
+            return this.taskDto.complete;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Task.prototype.completeChanged = function () {
+        this.taskDto.complete = this.completeInput.checked;
+        this.onCompleteChanged();
+    };
     Task.prototype.beginEdit = function () {
         this.editButton.style.display = "none";
         this.descriptionSpan.style.display = "none";
@@ -38,7 +56,7 @@ var Task = (function () {
         this.descriptionInput.select();
     };
     Task.prototype.save = function () {
-        this.descriptionSpan.innerHTML = this.descriptionInput.value;
+        this.descriptionSpan.innerHTML = this.taskDto.description = this.descriptionInput.value;
         this.editButton.style.display = "inline-block";
         this.descriptionSpan.style.display = "inline-block";
         this.descriptionInput.style.display = "none";
--- TypeScriptUiComponentSpike/TaskManager/Task.ts
+++ TypeScriptUiComponentSpike/TaskManager/Task.ts
@@ -5,6 +5,7 @@
  * Tasks can also notify others that they should be deleted.
  */
 class Task {
+	private completeInput: HTMLInputElement;
 	private descriptionSpan: HTMLSpanElement;
 	private descriptionInput: HTMLInputElement;
 	private editButton: HTMLButtonElement;
@@ -17,17 +18,19 @@ class Task {
 	 * @param {HTMLElement} An element that should act as a container for the task's UI.
 	 * @param {string} A description of the task.
 	 */
-	constructor(private container: HTMLElement, private description: string) {
+	constructor(private container: HTMLElement, private taskDto: ITaskDto, private onCompleteChanged: () => void) {
 		container.innerHTML = `
-<span>${description}</span>
-<input type="text" value="${description}" />
-<button data-role="edit">Edit</button>
-<button data-role="save">Save</button>
-<button data-role="cancel">Cancel</button>
+<input type="checkbox" class="one column" />
+<span class="seven columns">${taskDto.description}</span>
+<input type="text" class="seven columns" value="${taskDto.description}" />
+<button data-role="edit" class="four columns">Edit</button>
+<button data-role="save" class="two columns">Save</button>
+<button data-role="cancel" class="two columns">Cancel</button>
 `;
 
+		this.completeInput = <HTMLInputElement>container.querySelector("input[type=checkbox]");
 		this.descriptionSpan = <HTMLSpanElement>container.querySelector("span");
-		this.descriptionInput = <HTMLInputElement>container.querySelector("input");
+		this.descriptionInput = <HTMLInputElement>container.querySelector("input[type=text]");
 		this.editButton = <HTMLButtonElement>container.querySelector("button[data-role=edit]");
 		this.saveButton = <HTMLButtonElement>container.querySelector("button[data-role=save]");
 		this.cancelButton = <HTMLButtonElement>container.querySelector("button[data-role=cancel]");
@@ -36,9 +39,24 @@ class Task {
 		this.cancelButton.style.display = "none";
 		this.descriptionInput.style.display = "none";
 
+		this.completeInput.checked = taskDto.complete;
+
+		this.completeInput.addEventListener("click", () => this.completeChanged());
 		this.editButton.addEventListener("click", () => this.beginEdit());
 		this.saveButton.addEventListener("click", () => this.save());
 		this.cancelButton.addEventListener("click", () => this.cancelEdit());
+	}
+
+	/*
+	 * Gets a value indicating whether or not the task is complete.
+	 */
+	public get complete(): boolean {
+		return this.taskDto.complete;
+	}
+
+	private completeChanged() {
+		this.taskDto.complete = this.completeInput.checked;
+		this.onCompleteChanged();
 	}
 
 	private beginEdit() {
@@ -53,7 +71,7 @@ class Task {
 	}
 
 	private save() {
-		this.descriptionSpan.innerHTML = this.descriptionInput.value;
+		this.descriptionSpan.innerHTML = this.taskDto.description = this.descriptionInput.value;
 
 		this.editButton.style.display = "inline-block";
 		this.descriptionSpan.style.display = "inline-block";
